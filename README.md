007fe3c77485b2db3d06c74d24512bea2c2edb02
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-03-13 10:56:37 +0100

    Add all the files for thie project

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# capstone-project
+
+<h3 id = "readme-top"><b>Capstone project 1 </b></h3>
+<!-- TABLE OF CONTENTS -->
+
+# 📗 Table of Contents
+- [📖 Title](#Team Event)
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Key Features](#key-features)
+  - [🚀 Live Demo](#live-demo)
+- [👥 Authors](#authors)
+- [🔭 Future Features](#future-features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [❓ FAQ](#faq)
+- [📝 License](#license)
+
+
+# Team Event
+
+ You will learn how you can create enterprise value, empower greater worker productivity, and make the world a better place through advanced technology. This project is the result a my Capstone project 1
+
+## Built With
+
+- Html
+- Css
+- JavaScript
+
+### Home page
+
+[screenshot for desktop version](./img/desktop-img.png) 
+[screenshot for mobile version](./img/mobile-img.png)
+
+### Login window
+
+<!-- ![Popup screenshot](img/Popup.png) -->
+
+## Live Demo
+
+<!-- [Live Demo Link](https://barakadanny.github.io/capstone-project-1/) -->
+
+## Getting Started
+
+To run this project clone it with `git clone git@github.com:Amazinggracee/capstone-project.git`
+then run from a browser
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+- Browser eg. Chrome
+- Text Editor eg. Vs Code, Atom, Sublime text, etc.
+
+### Install
+
+Install linters to handle some common Errors, You can have description about [Linters here](https://github.com/microverseinc/linters-config)
+
+👤 **Amarachi Dimkpa**
+
+- GitHub: [@amazinggacee](https://github.com/Amazinggracee)
+- Twitter: [@amazinggaceu](https://twitter.com/amazinggraceu)
+- LinkedIn: [Amarachi Dimkpa](https://linkedin.com/in/amarachi-dimkpa-070643183)
+
+## Credit
+
+[@Cindy Shin](https://www.behance.net/adagio07) Thanks for this amazing design
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+<!-- Feel free to check the [issues page](https://github.com/barakadanny/capstone-project-1/issues). -->
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ main.js
@@ -0,0 +1,118 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.getElementById('navbar');
+const navbarMenu = document.querySelector('.navbar-right');
+const guestList = document.querySelector('.guest-list');
+const showMore = document.querySelector('.more-btn');
+
+// Feature data Object
+const speakers = [
+  {
+    speaker: 'MCKENZIE BRUNELLE',
+    role: 'Education Programs Lead',
+    description:
+      'McKenzie Brunelle is the PTC Education Partnerships and Outreach Programs Team Lead. She has been with PTC for four years. Following her passion for STEM education, she joined the education team as an Application Engineer. She now oversees content creation, competitions, and partner relations. She has a background in Biomedical Engineering and is currently pursuing her master’s in Learning Design and Technology from Purdue University.',
+    quotation: 'For your better tomorrow, save energy today.',
+    image: './img/mckenzie-brunelle.png',
+  },
+  {
+    speaker: 'LEILA COSTA',
+    role: ' sustainability manager for Sodexo (Atlanta)',
+    description:
+      'Leila Costa is the Director of ESG Metrics and Reporting at PTC. She is a graduate of the Yale School of the Environment. She has held leading positions in corporate sustainability, across industries, including consumer products, hospitality services, and now tech. Working on critical sustainability areas: carbon emissions, waste, hunger, diversity, equity, and inclusion, responsible sourcing, and ESG reporting and sustainability communications. ',
+    quotation: 'Save Energy, Save Money, Save Nation, Save the planet.',
+    image: './img/LEILA COSTA.jpg',
+  },
+  {
+    speaker: 'JONATHAN KATEMAN',
+    role: 'Chief of Digital Manufacturing, Rolls-Royce',
+    description:
+      'I am a leader who transforms complex organisations to be more competitive and sustainable through technology. I lead global technology teams to bring business strategies, operational requirements, and innovative technologies together to create real business value and transformation.',
+    quotation: 'Energy saved is Energy generated.',
+    image: './img/Constant_Contact_Jonathan_Kateman.jpg',
+  },
+  {
+    speaker: 'JENNY JOHNSON',
+    role: 'Cloud Infrastructure, Onshape',
+    description:
+      'Jenny Johnson, Principal Technical Services Engineer is a Principal Technical Services Engineer, fulfilling both a PreSales and Post Sales role. She has over 20 years of industry experience before joining the CAD industries. She previously was a PDM project manager and helped hundreds of companies shape their PDMPLM processes.',
+    quotation: 'Reduce, Reuse and Recycle',
+    image: './img/Jenny-Johnson-scaled.jpg',
+  },
+  {
+    speaker: ' AMARACHI DIMKPA',
+    role: 'Software Engineer',
+    description:
+      'Hello Im a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.',
+    quotation: 'Rock your world by saving energy.',
+    image: './img/Amara.png',
+  },
+  {
+    speaker: 'Golda Onyia',
+    role: 'Business Development Associate',
+    description:
+      'Golda Onyia is a Business Development Associate from Amokwe Udi near Enugu.  She has been with Solar Sister since 2021. Golda studied Chemical Engineering in school and speaks English and Igbo languages. Her goal is to be a mentor to young women.',
+    quotation: 'Think. Act. Save.',
+    image: './img/Golda.jpg',
+  },
+];
+
+// burger menu action
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+// glue navigation menu
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.add('glue');
+  } else {
+    navbar.classList.remove('glue');
+  }
+};
+
+// open menu on click
+menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  navbarMenu.classList.toggle('show-menu');
+});
+
+// generate Dynamic guest list
+for (let i = 0; i < speakers.length; i += 1) {
+  const specialGuest = speakers[i];
+
+  const guest = document.createElement('guest');
+  guest.classList.add('guest');
+  guest.innerHTML = `
+    <div class="guest-item">
+        <div class="guest-item-img">
+            <img src="${specialGuest.image}" alt="">
+        </div>
+        <div class="guest-item-text">
+            <h3>${specialGuest.speaker}</h3>
+            <span class="role">${specialGuest.role} </span>
+            <p>
+                ${specialGuest.description}
+            </p>
+            <q>${specialGuest.quotation}</q>
+        </div>
+    </div>
+  `;
+  guestList.appendChild(guest);
+}
+
+// unhide guest List on click
+showMore.addEventListener('click', (e) => {
+  e.preventDefault();
+  guestList.classList.toggle('hide');
+  showMore.textContent = showMore.textContent.includes('See More')
+    ? 'See Less'
+    : 'See More';
+});
